7df8c2db8bf381db6be43bf3053a4831685e34a6
lukstafi <lukstafi@gmail.com>
2023-03-01 21:10:26 +0100

    In progress: migrating to `ptime`

--- debug_runtime_jane.ml
+++ debug_runtime_jane.ml
@@ -1,18 +1,18 @@
 open Base
 
-module Printf(File_name: sig val v : string end) = struct
+module Printf(File_name: sig val v : string end) = struct  
   let debug_ch =
-    let debug_ch =
-      Stdio.Out_channel.create ~binary:false ~append:true File_name.v in
-    Stdio.Out_channel.fprintf debug_ch "\nBEGIN DEBUG SESSION at time UTC %s\n%!"
-      (Core.Time_ns.to_string_utc @@ Core.Time_ns.now());
-    debug_ch
+    Stdio.Out_channel.create ~binary:false ~append:true File_name.v
   let ppf =
     let ppf = Caml.Format.formatter_of_out_channel debug_ch in
     Caml.Format.pp_set_geometry ppf ~max_indent:50 ~margin:100;
     ppf
-  let pp_printf () (type a): (a, Caml.Format.formatter, unit) format -> a =
-    Caml.Format.fprintf ppf
+  let pp_timestamp ppf now =
+    let tz_offset_s = Ptime_clock.current_tz_offset_s () in
+    Ptime.(pp_human ?tz_offset_s ()) ppf now
+  let () =
+    Caml.Format.fprintf ppf "\nBEGIN DEBUG SESSION at time %a\n%!"
+    pp_timestamp (Ptime_clock.now ())
   let open_box() = Caml.Format.pp_open_hovbox ppf 2
   let close_box ~toplevel () =
     Caml.Format.pp_close_box ppf ();
