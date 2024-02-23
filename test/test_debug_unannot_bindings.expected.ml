573b944b4fdff28dd2b9d7e2a43e1ef9e10164b3
Lukasz Stafiniak <lukstafi@gmail.com>
2024-02-23 10:26:51 +0100

    Fix build for the new ppx expansion test

--- test/test_debug_unannot_bindings.expected.ml
+++ test/test_debug_unannot_bindings.expected.ml
@@ -1,11 +1,11 @@
 module Debug_runtime = (Minidebug_runtime.Flushing)((val
   Minidebug_runtime.debug_ch "debugger_unannot_bindings.log"))
-let result =
+let _result =
   let __entry_id = Debug_runtime.get_entry_id () in
   ();
   Debug_runtime.open_log_preamble_brief
     ~fname:"test_debug_unannot_bindings.ml" ~pos_lnum:5 ~pos_colnum:15
-    ~message:"result" ~entry_id:__entry_id;
+    ~message:"_result" ~entry_id:__entry_id;
   (match let a = 1 in let b = 2 in let point = (a, b) in ignore point with
    | _ as __res -> ((); Debug_runtime.close_log (); __res)
    | exception e -> (Debug_runtime.close_log (); raise e))
--- test/test_debug_unannot_bindings.ml
+++ test/test_debug_unannot_bindings.ml
@@ -2,7 +2,7 @@ module Debug_runtime =
   Minidebug_runtime.Flushing
     ((val Minidebug_runtime.debug_ch "debugger_unannot_bindings.log"))
 
-let%debug_show result =
+let%debug_show _result =
   let a = 1 in
   let b = 2 in
   let%ppx_minidebug_noop_for_testing point =
